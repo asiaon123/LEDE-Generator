5eb95f2dcca5e8465a1bc3001859d26784016d48
Yaalon <asiaon123@gmail.com>
2021-02-19 23:39:11 +0800

    Add 1806 build

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.121.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='ZackModOpenWrt'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/lean/default-settings/files/zzz-default-settings
