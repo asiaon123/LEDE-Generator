7e274b92d9f594cabe94b624e78839793939630f
Zack <asiaon123@gmail.com>
2021-05-20 23:40:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,8 +8,11 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
+# Modify Default Router IP
 sed -i 's/192.168.1.1/192.168.121.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/system.@system[-1].hostname='\''ImmortalWrt'\''/system.@system[-1].hostname='\''OpenWrt'\''/g' package/base-files/files/bin/config_generate
+
+#Modify Default Lang
+sed -i 's/uci set luci.main.lang=auto/uci set luci.main.lang=zh_cn/g' package/emortal/default-settings/files/zzz-default-settings
