e8bfae21460d008f589586283db353513d6f973d
Zack Gu <asiaon123@gmail.com>
2021-05-24 10:52:28 +0800

    Try Fix

--- custom.sh
+++ custom.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
 
 # Modify Default Router IP
 sed -i 's/192.168.1.1/192.168.121.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/system.@system[-1].hostname='\''ImmortalWrt'\''/system.@system[-1].hostname='\''OpenWrt'\''/g' package/base-files/files/bin/config_generate
+# Modify hostname in chinese
+sed -i '/uci commit system/i\uci set system.@system[0].hostname=OpenWrt' package/emortal/default-settings/files/zzz-default-settings-chinese
 
 #Modify Default Lang
 sed -i 's/uci set luci.main.lang=auto/uci set luci.main.lang=zh_cn/g' package/emortal/default-settings/files/zzz-default-settings
+
+# Modify Openwrt Banner
+sed -i 's/ImmortalWrt/OpenWrt/g' package/emortal/default-settings/files/openwrt_banner
