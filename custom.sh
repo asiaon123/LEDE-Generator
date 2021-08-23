882627a1526be3a149376b7c59591f817841a751
Zack <asiaon123@gmail.com>
2021-08-23 15:45:16 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -4,7 +4,7 @@
 sed -i "s/192.168.1.1/$2/" package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i "s/system.@system[-1].hostname='ImmortalWrt'/system.@system[-1].hostname='$1'/g" package/base-files/files/bin/config_generate
+sed -i "s/ImmortalWrt/$1/g" package/base-files/files/bin/config_generate
 # Modify hostname in chinese
 sed -i "/uci commit system/i\uci set system.@system[0].hostname=$1" package/emortal/default-settings/files/zzz-default-settings-chinese
 
